0e7685f663416cee009a6c4c2cee93abb2f5a11e
Fu Xing <bio.xfu@gmail.com>
2018-03-16 09:06:19 +0800

    module add

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 source activate gmatic
 conda env export > doc/environment.yml
+module add bcftools/0.1.19
+module add samtools/0.1.19
 
 if [ ! -d fastq ]; then
 	mkdir -p bam clean fastq fastqc/raw fastqc/clean output stat track figures RData
