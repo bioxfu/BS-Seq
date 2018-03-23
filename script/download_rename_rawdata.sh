6fb6267f15b7be45b970809eebe29176ed3eb35d
Fu Xing <bio.xfu@gmail.com>
2018-03-23 16:22:40 +0800

    add download and rename function

--- script/download_rename_rawdata.sh
+++ script/download_rename_rawdata.sh
@@ -1,7 +1,7 @@
-###cd /var/data/raw_data/ZHM/CSL/
-scp -r lpeng@10.41.25.251:$1 ./
-ls */*/* |./rush 'mv {} {/%}_R{%@.+_(\d).fq.gz}.fastq.gz' --dry-run
-ls */*/* |./rush 'mv {} {/%}_R{%@.+_(\d).fq.gz}.fastq.gz'
+scp -r xfu@10.41.25.251:$1 ./
+dir=`echo $1|sed -r 's/.*\///g'`
+ls $dir/*/* |./script/rush -k 'mv {} {/%}_{%@.+_(R\d).fq.gz}.fastq.gz' --dry-run
+ls $dir/*/* |./script/rush -k 'mv {} {/%}_{%@.+_(R\d).fq.gz}.fastq.gz'
 mkdir fastq
 mv *.fastq.gz fastq
 
