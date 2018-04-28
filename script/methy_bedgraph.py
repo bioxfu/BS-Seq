f243ecfad097e3234be4e85fa10bee9d2b0f74cb
Fu Xing <bio.xfu@gmail.com>
2018-04-28 09:59:03 +0800

    set coverage cutoff = 10

--- script/methy_bedgraph.py
+++ script/methy_bedgraph.py
@@ -3,23 +3,20 @@
 import sys
 import gzip
 
-output_file = open(sys.argv[2], 'w')
-output_file.write('track type=bedGraph\n')
+cov_cutoff = 10
 
 with gzip.open(sys.argv[1], 'rb') as f:
 	for line in f:
 		line = line.decode('utf-8')
 		lst = line.strip().split('\t')
 		chrom = lst[0]
-		pos = int(lst[1])
+		pos = int(lst[1]) # 1-based
 		strand = lst[2]
 		methy_count = float(lst[3])
 		unmethy_count = float(lst[4])
 		total_count = methy_count + unmethy_count
-		if total_count > 0 and methy_count > 0:
+		if total_count >= cov_cutoff:
 			ratio = methy_count / total_count
 			if strand == '-':
 				ratio = -1 * ratio
-			output_file.write('%s\t%s\t%s\t%.3f\n' % (chrom, pos, pos+1, ratio))
-
-output_file.close()
+			print('%s\t%s\t%s\t%.3f' % (chrom, pos-1, pos, ratio))
